438f0a5bf0ee018365b8d11e17440c721dc5b5be
DirtyBoy <49908353+Dirtyboy1029@users.noreply.github.com>
2025-01-22 12:26:58 +0800

    Update collect_non_member.py

--- sota_tool/build_dataset/collect_non_member.py
+++ sota_tool/build_dataset/collect_non_member.py
@@ -7,10 +7,10 @@
 import os, random
 from datasets import Dataset
 
-# 设置路径
-books_dir = "/opt/data/private/LHD_LLM/LLM_uncertainty/bayesian_peft/Database/deepmind-gutenberg/eBooks"  # 修改为你的书籍文件夹路径
 
-# 创建数据集列表
+books_dir = "/opt/data/private/LHD_LLM/LLM_uncertainty/bayesian_peft/Database/deepmind-gutenberg/eBooks"  
+
+
 data = []
 data_num = len(os.listdir(books_dir))
 need_id = random.sample(list(range(data_num)), k=2500)
@@ -20,19 +20,18 @@ for book_id, filename in enumerate(os.listdir(books_dir)):
         if filename.endswith('.txt'):
             book_path = os.path.join(books_dir, filename)
 
-            # 读取文本文件内容
+       
             with open(book_path, 'r', encoding='utf-8') as f:
                 text = f.read()
 
             book = {
-                "book": filename,  # 文件名作为书名（去掉后缀）
-                "bookid": book_id + 1,  # 假设书的ID是按顺序排列的
+                "book": filename,  
+                "bookid": book_id + 1, 
                 "text": text,
                 "label": 0
             }
             data.append(book)
 
-# 创建 Hugging Face Dataset
 dataset = Dataset.from_dict({
     "book": [entry["book"] for entry in data],
     "bookid": [entry["bookid"] for entry in data],
@@ -40,8 +39,6 @@ dataset = Dataset.from_dict({
     "label": [entry["label"] for entry in data]
 })
 
-# 查看前几条数据
 print(dataset[:5])
 
-# 保存数据集为 `.hg` 格式
 dataset.save_to_disk('/opt/data/private/LHD_LLM/LLM_uncertainty/bayesian_peft/Database/pg19_gutenberg/non_member')
