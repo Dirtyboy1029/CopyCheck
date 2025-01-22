b494861f14d0c1a6814a9eb4530d43e88ed371bd
DirtyBoy <49908353+Dirtyboy1029@users.noreply.github.com>
2025-01-22 12:23:17 +0800

    Update my_script.sh

--- my_bayesian_peft/scripts/my_script.sh
+++ my_bayesian_peft/scripts/my_script.sh
@@ -4,7 +4,7 @@ for epoch in 1
 do
     CUDA_VISIBLE_DEVICES=0 python3 run/main.py --dataset-type mcdataset --dataset $dataset --model-type causallm \
     --model openlm-research/open_llama_7b --modelwrapper mcdropout --lr 1e-4 --batch-size 4  --opt adamw --warmup-ratio 0.06 \
-    --max-seq-len 300  --nowand --load-model-path /opt/data/private/LHD_LLM/LLM_uncertainty/my_llm/openlm-research/open_llama_7b \
+    --max-seq-len 300  --nowand --load-model-path ./my_llm/openlm-research/open_llama_7b \
     --apply-classhead-lora --lora-r 8 --lora-alpha 16 --lora-dropout 0.1  \
      --checkpoint --checkpoint-dic-name epoch$epoch/open_llama_7b-1 --seed $epoch --n-epochs $epoch
 done
@@ -31,7 +31,7 @@ for epoch in 1
 do
     CUDA_VISIBLE_DEVICES=0 python3 run/main.py --dataset-type mcdataset --dataset $dataset --model-type causallm \
     --model openlm-research/open_llama_7b --modelwrapper blob --lr 1e-4 --batch-size 4  --opt adamw --warmup-ratio 0.06 \
-    --max-seq-len 300  --nowand --load-model-path /opt/data/private/LHD_LLM/LLM_uncertainty/my_llm/openlm-research/open_llama_7b \
+    --max-seq-len 300  --nowand --load-model-path ./my_llm/openlm-research/open_llama_7b \
     --apply-classhead-lora --lora-r 8 --lora-alpha 16 --lora-dropout 0  --bayes-klreweighting  \
     --bayes-eps 0.05 --bayes-beta 0.2 --bayes-gamma 8 --bayes-kllr 0.01 --bayes-train-n-samples 1 \
     --bayes-final-beta 0.18 --checkpoint --checkpoint-dic-name epoch$epoch/open_llama_7b-1 --seed 1234 --n-epochs $epoch
@@ -56,7 +56,7 @@ do
     CUDA_VISIBLE_DEVICES=0  python3.11 run/main.py --dataset-type mcdataset --dataset $dataset \
     --model-type causallm --model openlm-research/open_llama_7b --modelwrapper deepensemble  --lr 1e-4 --batch-size 4  \
     --opt adamw --warmup-ratio 0.06  --max-seq-len 300  --nowand  --apply-classhead-lora --lora-r 8 --lora-alpha 16 \
-    --lora-dropout 0   --load-model-path /opt/data/private/LHD_LLM/LLM_uncertainty/my_llm/openlm-research/open_llama_7b \
+    --lora-dropout 0   --load-model-path ./my_llm/openlm-research/open_llama_7b \
     --checkpoint --checkpoint-dic-name epoch$epoch/open_llama_7b-10 --ensemble-n 10 --seed $epoch --n-epochs $epoch
 done
 done
